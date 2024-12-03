ee0e7cb21225bcb2926449ed7fa2e4d66be77e2d
azeem1234coder <odusanyaabdulazeem25@gmail.com>
2024-12-03 21:20:22 +0100

    2nd

---
+++ next.py
@@ -0,0 +1,18 @@
+name="penguin"
+age=15
+is_student=True
+weight=38.5
+print("Name :", name)
+print("Data Type of Name is",type(name))
+print("Age :",age)
+print("Data type of Age is",type(age))
+print("is_student :",is_student)
+print("Weight :",weight)
+print("Data Types of weight is",type(weight))
+print("\n After Type Casting.....")
+age=str(age)
+print(age)
+print("Data Type of age is",type(age))
+weight=int(weight)
+print(weight)
+print("Data Type of weight is ",type(weight))
